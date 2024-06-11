72a4050624113edfd1a6644ca78f205c830eaa5a
Paulo Henrique Duarte <igrsysten@gmail.com>
2024-06-11 08:21:23 -0300

    Create Public

---
+++ public/assets/js/pages/new-widgets.js
@@ -0,0 +1,20 @@
+// card js plugin
+var card = new Card({
+    form: document.querySelector('#card-form-elem'),
+    container: '.card-wrapper',
+    formSelectors: {
+        numberInput: 'input#card-number-input',
+        expiryInput: 'input#card-expiry-input',
+        cvcInput: 'input#card-cvc-input',
+        nameInput: 'input#card-name-input'
+    },
+    placeholders: {
+        number: '•••• •••• •••• ••••',
+        name: 'Full Name',
+        expiry: '••/••',
+        cvc: '•••'
+    },
+    masks: {
+        cardNumber: '•' // optional - mask card number
+    },
+});
