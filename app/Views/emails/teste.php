661a3670bb55d160b488e6102f8000e8a3f3db34
Paulo Henrique Duarte <igrsysten@gmail.com>
2024-06-26 04:07:44 -0300

    Update teste.php

--- app/Views/emails/teste.php
+++ app/Views/emails/teste.php
@@ -11,7 +11,7 @@
                                 <tr style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
                                     <td class="content-block" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
                                         <div style="text-align: center;margin-bottom: 15px;">
-                                            <img src="<?= site_url($logo) ?>" alt="" height="23">
+                                            <img src="<?= $logo ?>" alt="" height="23">
                                         </div>
                                     </td>
                                 </tr>
