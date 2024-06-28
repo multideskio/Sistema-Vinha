fc2f2e5aba4bec56be38e036acff53588ddeb285
Paulo Henrique Duarte <igrsysten@gmail.com>
2024-06-28 03:33:45 -0300

    Update home.php

--- app/Views/admin/pages/home.php
+++ app/Views/admin/pages/home.php
@@ -248,7 +248,7 @@
                         <div class="card-body">
                             <div class="d-flex align-items-center">
                                 <div class="flex-grow-1 overflow-hidden">
-                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Pessoas</p>
+                                    <p class="text-uppercase fw-medium text-muted text-truncate mb-0">Usuários</p>
                                 </div>
                                 <div class="flex-shrink-0">
 
