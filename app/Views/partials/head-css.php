ee414448554467cce2af320e5dc09bfc7099b39e
Paulo Henrique Duarte <igrsysten@gmail.com>
2024-06-29 16:14:27 -0300

    PWA

--- app/Views/partials/head-css.php
+++ app/Views/partials/head-css.php
@@ -14,21 +14,15 @@
 </script>
 
 <style>
-    /* Estilo para dispositivos móveis */
-    .pwa-install-prompt {
+    button.install {
         position: fixed;
         bottom: 10px;
-        left: 50%;
-        transform: translateX(-50%);
-        background-color: #644cc1;
-        /* Cor de fundo da notificação */
+        right: 10px;
+        padding: 10px 20px;
+        background-color: #000000;
         color: #ffffff;
-        /* Cor do texto da notificação */
-        padding: 10px;
-        text-align: center;
-        z-index: 1000;
-        /* Certifique-se de que está acima de outros elementos */
-        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-        /* Sombra para destacar a notificação */
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
     }
 </style>
