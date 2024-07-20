c13fce4df267945f0fab79cda13c60f80f041ca3
Paulo Henrique Duarte <multidesk.io@gmail.com>
2024-07-20 09:58:59 -0300

    Update footer.php

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -6,7 +6,7 @@
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Design & Develop by Multidesk.io
+                    Design & Develop by Multidesk.io - <b>{elapsed_time}</b>
                 </div>
             </div>
         </div>
