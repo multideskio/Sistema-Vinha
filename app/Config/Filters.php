1d4d974383298e760c335e9359118f058bc56633
Paulo Henrique Duarte <multidesk.io@gmail.com>
2024-07-20 10:45:31 -0300

    Update Filters.php

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -48,6 +48,7 @@ class Filters extends BaseConfig
         ],
         'after' => [
             'toolbar',
+            'performance'
             // 'honeypot',
             // 'secureheaders',
         ],
