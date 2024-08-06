f71afe7a997d23e7948e23412dc2a54cbff923d7
Paulo Henrique Duarte <multidesk.io@gmail.com>
2024-08-06 04:16:50 -0300

    Update altera.php

--- app/Views/admin/pages/gerentes/altera.php
+++ app/Views/admin/pages/gerentes/altera.php
@@ -97,7 +97,7 @@
             <div class="card-body p-4">
                 <div class="tab-content">
                     <div class="tab-pane active" id="personalDetails" role="tabpanel">
-                        <?= form_open('api/v1/gerentes/' . $idSearch, 'class="formGeral"') ?>
+                        <?= form_open('api/v1/gerentes/' . $idSearch, 'class="formGeral" autocomplete="off"') ?>
                         <div class="row">
                             <div class="col-lg-4">
                                 <div class="mb-3">
