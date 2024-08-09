d089b1a0a217cde778107f4c0223abed89b274f2
Paulo Henrique Duarte <multidesk.io@gmail.com>
2024-08-09 11:26:02 -0300

    Load css

--- app/Views/partials/head-css.php
+++ app/Views/partials/head-css.php
@@ -13,55 +13,13 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 <script src="https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js"></script>
+
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css">
+
+<link src="/assets/css/custom/load.min.css">
 
 <!-- Variaveis -->
 <script>
     const _baseUrl = "<?= site_url() ?>"
 </script>
 
-<style>
-    #pwa-install-banner {
-        display: none;
-        position: fixed;
-        top: 0;
-        width: 100%;
-        background-color: #4CAF50;
-        color: white;
-        text-align: center;
-        padding: 1em;
-        z-index: 1000;
-    }
-
-    #pwa-install-banner button {
-        background-color: #fff;
-        color: #4CAF50;
-        border: none;
-        padding: 0.5em 1em;
-        margin-left: 1em;
-        cursor: pointer;
-    }
-
-    .pace {
-        -webkit-pointer-events: none;
-        pointer-events: none;
-
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-    }
-
-    .pace-inactive {
-        display: none;
-    }
-
-    .pace .pace-progress {
-        background: #229fdd;
-        position: fixed;
-        z-index: 2000;
-        top: 0;
-        right: 100%;
-        width: 100%;
-        height: 2px;
-    }
-</style>
